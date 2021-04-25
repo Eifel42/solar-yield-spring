01eb489b05aa74181dbe407d6e991981d06de4a0
Stefan Zils <stefan.zils@inoatec.de>
2021-04-25 11:47:28 +0200

    init

---
+++ yield-data/src/main/java/de/inoatec/solar/yielddata/SolarYieldDataApplication.java
@@ -0,0 +1,13 @@
+package de.inoatec.solar.yielddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarYieldDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarYieldDataApplication.class, args);
+    }
+
+}
---
+++ yield-data/src/test/java/de/inoatec/solar/yielddata/SolarYieldDataApplicationTests.java
@@ -0,0 +1,13 @@
+package de.inoatec.solar.yielddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarYieldDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
